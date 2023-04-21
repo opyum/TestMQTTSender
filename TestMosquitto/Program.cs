f63dba63e60f9fcb4f176b242a0159c30bd6246c
Florian Lorca <florca-ext@citegestion.fr>
2023-04-21 15:06:00 +0200

    first commit

---
+++ TestMosquitto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using MQTTnet;
+using MQTTnet.Client;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new MqttFactory();
+        var client = factory.CreateMqttClient();
+
+        var options = new MqttClientOptionsBuilder()
+            .WithTcpServer("host.docker.internal") // replace with the address of your MQTT 
+            .WithCredentials("snefUser", "Sn3fL@b!26") // replace with your credentials
+            .WithClientId("Dev_Send_MQPub") // replace with a client ID of your choice
+            .Build();
+
+        await client.ConnectAsync(options, CancellationToken.None);
+
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("internal-exchange/update-car") // replace with the topic you want to publish to
+            .WithPayload("Hello world!") // replace with the message you want to publish
+            .Build();
+
+        while (true)
+        {
+            await client.PublishAsync(message, CancellationToken.None);
+            Thread.Sleep(1000);
+        }
+
+        await client.DisconnectAsync();
+    }
+}
